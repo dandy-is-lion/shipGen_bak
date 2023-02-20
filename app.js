a516f5a432ae1e035ae83f4d3c0605c02ac81897
dandyislion <dandyislion@protonmail.com>
2023-02-20 08:52:48 -0600

    Queries implemented

--- app.js
+++ app.js
@@ -1,3 +1,69 @@
+function runQuery(query) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var results = JSON.parse(xhr.responseText);
+                var cols = results["columns"];
+                var data = results["data"];
+                var html = "<table class='results-table' id='results-table'><thead><tr>";
+                for (var i = 0; i < cols.length; i++) {
+                    html += "<th title='" + cols[i].replaceAll('_', ' ') + "'>" + cols[i].replaceAll('_', ' ') + "</th>";
+                }
+                html += "</tr></thead><tbody>";
+                for (var i = 0; i < data.length; i++) {
+                    html += "<tr>";
+                    for (var j = 0; j < cols.length; j++) {
+                        html += "<td title='" + cols[j].replaceAll('_', ' ') + ": " + data[i][cols[j]] + "'>" + data[i][cols[j]] + "</td>";
+                    }
+                    html += "</tr>";
+                }
+                html += "</tbody></table>";
+                document.getElementById("results-shipgen").innerHTML = html;
+                addRowHandlers();
+            } else {
+                alert("No results found.");
+            }
+        }
+    };
+    xhr.open("GET", "query.php?query=" + encodeURIComponent(query), true);
+    xhr.send();
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    // var query = "SELECT * FROM combos_";
+    // const score = document.getElementById("score").value;
+    // const power_min = document.getElementById("power_min").value;
+    // const power_max = document.getElementById("power_max").value;
+    // const ship = document.getElementById("ship").selectedOptions;
+    // var ships = [];
+    // for (let i = 0; i < ship.length; i++) {
+    //     ships.push(ship[i].value);
+    // }
+    // const limit = document.getElementById("limit").value;
+    // if (score == "all") {
+    //     query += score + " WHERE score BETWEEN " + power_min + " AND " + power_max;
+    // } else {
+    //     query += score + " WHERE score = score";
+    // }
+    // if (!ships.includes('any')) {
+    //     query += " AND ship IN (" + ships + ")";
+    // }
+    // query += " LIMIT " + limit;
+    runQuery(document.getElementById("query").value);
+}
+
+Chart.defaults.borderColor = '#3B8EB9';
+Chart.defaults.color = '#FFF';
+Chart.defaults.responsive = false;
+Chart.defaults.maintainAspectRatio = true;
+Chart.defaults.plugins.decimation = false;
+
+var dataSelection = [0, 0, 0, 0, 0, 0];
+var dataComparison = [0, 0, 0, 0, 0, 0];
+var dataTarget = [39, 43, 42, 37, 42, 41];
+
 const statLabels = [
     'Durability',
     'Thrust',
@@ -7,13 +73,14 @@ const statLabels = [
     'Strafe'
 ];
 
-var statData = [{
+const statData = [{
     label: 'Selection',
-    data: [0, 0, 0, 0, 0, 0],
+    data: dataSelection,
     fill: true,
     borderWidth: 3,
     backgroundColor: 'rgba(0, 72, 186, 0.2)',
     borderColor: 'rgb(0, 72, 186)',
+    pointHitRadius: 25,
     pointBorderWidth: 2,
     pointBackgroundColor: 'rgb(0, 72, 186)',
     pointBorderColor: '#fff',
@@ -21,11 +88,12 @@ var statData = [{
     pointHoverBorderColor: 'rgb(0, 72, 186)'
 }, {
     label: 'Comparison',
-    data: [0, 0, 0, 0, 0, 0],
+    data: dataComparison,
     fill: true,
     borderWidth: 3,
     backgroundColor: 'rgba(229,43,80, 0.2)',
     borderColor: 'rgb(229,43,80)',
+    pointHitRadius: 25,
     pointBorderWidth: 2,
     pointBackgroundColor: 'rgb(229,43,80)',
     pointBorderColor: '#fff',
@@ -33,11 +101,12 @@ var statData = [{
     pointHoverBorderColor: 'rgb(229,43,80)'
 }, {
     label: 'Target',
-    data: [0, 0, 0, 0, 0, 0],
+    data: dataTarget,
     fill: true,
     borderWidth: 3,
     backgroundColor: 'rgba(153, 255, 51, 0.2)',
     borderColor: 'rgb(153, 255, 51)',
+    pointHitRadius: 25,
     pointBorderWidth: 2,
     pointBackgroundColor: 'rgb(153, 255, 51)',
     pointBorderColor: '#fff',
@@ -45,28 +114,8 @@ var statData = [{
     pointHoverBorderColor: 'rgb(153, 255, 51)'
 }];
 
-const ctxBars = document.getElementById('stat-bars');
-const ctxRadar = document.getElementById('stat-radar');
-
-var statBars = new Chart(ctxBars, {
-    type: 'bar',
-    data: {
-        labels: statLabels,
-        datasets: statData
-    },
-    options: {
-        legend: {
-            display:false,
-        },
-        scales: {
-            x: {
-                grid: {
-                    offset: true
-                }
-            }
-        }
-    },
-});
+const ctxRadar = document.getElementById('stat-radar').getContext('2d');
+const ctxBars = document.getElementById('stat-bars').getContext('2d');
 
 var statRadar = new Chart(ctxRadar, {
     type: 'radar',
@@ -75,14 +124,160 @@ var statRadar = new Chart(ctxRadar, {
         datasets: statData
     },
     options: {
+        onHover: function (e) {
+            const point = e.chart.getElementsAtEventForMode(e, 'nearest', {
+                intersect: true
+            }, false)
+            if (point.length) e.native.target.style.cursor = 'grab'
+            else e.native.target.style.cursor = 'default'
+        },
+        plugins: {
+            dragData: {
+                round: 1,
+                showTooltip: true,
+                onDragStart: function (e) {
+                    if (!(element.datasetIndex === 2)) return false
+                },
+                onDrag: function (e, datasetIndex, index, value) {
+                    e.target.style.cursor = 'grabbing'
+                    if (!(datasetIndex === 2)) return false
+                },
+                onDragEnd: function (e, datasetIndex, index, value) {
+                    e.target.style.cursor = 'default'
+                    statBars.update();
+                },
+                magnet: {
+                    to: Math.round
+                }
+            }
+        },
         scales: {
             r: {
-                angleLines: {
-                    display: false
+                color: '#fff',
+                suggestedMax: 50,
+                min: 0,
+                beginAtZero: true,
+                ticks: {
+                    showLabelBackdrop: true,
+                    backdropColor: 'rgba(0,0,0,0.2)',
+                    z: 0
                 },
-                suggestedMin: 0,
-                suggestedMax: 100
+                pointLabels: {
+                    centerPointLabels: false,
+                    padding: 0,
+                    display: true,
+                    font: {
+                        weight: 600,
+                        size: 11
+                    }
+                }
             }
         }
     },
 });
+
+var statBars = new Chart(ctxBars, {
+    type: 'bar',
+    data: {
+        labels: statLabels,
+        datasets: statData
+    },
+    options: {
+        scales: {
+            y: {
+                suggestedMax: 100,
+                min: 0,
+                beginAtZero: true
+            }
+        },
+        onHover: function (e) {
+            const point = e.chart.getElementsAtEventForMode(e, 'nearest', {
+                intersect: true
+            }, false)
+            if (point.length) e.native.target.style.cursor = 'grab'
+            else e.native.target.style.cursor = 'default'
+        },
+        plugins: {
+            dragData: {
+                round: 1,
+                showTooltip: true,
+                onDragStart: function (e) {
+                    if (!(element.datasetIndex === 2)) return false
+                },
+                onDrag: function (e, datasetIndex, index, value) {
+                    e.target.style.cursor = 'grabbing'
+                    if (!(datasetIndex === 2)) return false
+                },
+                onDragEnd: function (e, datasetIndex, index, value) {
+                    e.target.style.cursor = 'default'
+                    statRadar.update();
+                },
+                magnet: {
+                    to: Math.round // to: (value) => value + 5
+                }
+            }
+        },
+    },
+});
+
+function updateStatCharts(chart, dataset, data) {
+    chart.data.datasets[dataset].data = data;
+    chart.update();
+}
+
+function getStats(row) {
+    return [Math.max(0, getCellValue(row, 9)),
+        Math.max(0, getCellValue(row, 10)),
+        Math.max(0, getCellValue(row, 11)),
+        Math.max(0, getCellValue(row, 12)),
+        Math.max(0, getCellValue(row, 13)),
+        Math.max(0, getCellValue(row, 14))
+    ];
+}
+
+function getCellValue(row, cell) {
+    return row.getElementsByTagName('td')[cell].innerHTML;
+}
+
+function addRowHandlers() {
+    var table = document.getElementById("results-table");
+    var rows = table.getElementsByTagName("tr");
+    for (i = 1; i < rows.length; i++) {
+        var currentRow = table.rows[i];
+        var createClickHandler = function (row) {
+            return function () {
+                var stats = getStats(row);
+                updateStatCharts(statRadar, 0, stats);
+                updateStatCharts(statBars, 0, stats);
+            };
+        };
+        var createHoverHandler = function (row) {
+            return function () {
+                var stats = getStats(row);
+                updateStatCharts(statRadar, 1, stats);
+                updateStatCharts(statBars, 1, stats);
+            };
+        };
+        currentRow.onclick = createClickHandler(currentRow);
+        currentRow.addEventListener("mouseover", createHoverHandler(currentRow));
+    }
+}
+
+//   const queryDurability = document.getElementById('durability');
+//   const queryThrust = document.getElementById('thrust');
+//   const queryTopSpeed = document.getElementById('top_speed');
+//   const queryStability = document.getElementById('stability');
+//   const querySteer = document.getElementById('steer');
+//   const queryStrafe = document.getElementById('strafe');
+
+//   var createTargetHandler = function () {
+//     return function () {
+//       updateStatCharts(statRadar, 2, [queryDurability.value,
+//         queryThrust.value,
+//         queryTopSpeed.value,
+//         queryStability.value,
+//         querySteer.value,
+//         queryStrafe.value
+//       ])
+//     }
+//   };
